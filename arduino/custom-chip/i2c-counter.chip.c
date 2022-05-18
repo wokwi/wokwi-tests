2b2710c79949f85fc41288171db1575771fee259
Uri Shaked <uri@urishaked.com>
2022-05-18 10:38:14 +0300

    fix(custom-chip): update code to latest API

--- arduino/custom-chip/i2c-counter.chip.c
+++ arduino/custom-chip/i2c-counter.chip.c
@@ -22,13 +22,27 @@ typedef struct {
   uint32_t threshold_attr;
 } chip_state_t;
 
-chip_state_t* EXPORT(chip_init)(void) {
+static bool on_i2c_connect(void *chip, uint32_t address, bool connect);
+static uint8_t on_i2c_read(void *chip);
+static bool on_i2c_write(void *chip, uint8_t data);
+static void on_i2c_disconnect(void *chip);
+
+void* chip_init() {
   chip_state_t *chip = malloc(sizeof(chip_state_t));
-  const pin_t scl = pin_init("SCL", INPUT);
-  const pin_t sda = pin_init("SDA", INPUT);
+
   chip->pin_int = pin_init("INT", INPUT);
-  i2c_init(ADDRESS, scl, sda);
   chip->counter = 0;
+
+  const i2c_dev_t i2c_dev = {
+    .address = ADDRESS,
+    .scl = pin_init("SCL", INPUT),
+    .sda = pin_init("SDA", INPUT),
+    .connect = on_i2c_connect,
+    .read = on_i2c_read,
+    .write = on_i2c_write,
+    .disconnect = on_i2c_disconnect, // Optional
+  };
+  i2c_init(chip, &i2c_dev);
 
   // This attribute can be edited by the user. It's defined in wokwi-custom-part.json:
   chip->threshold_attr = attr_init("threshold", 127);
@@ -36,35 +50,38 @@ chip_state_t* EXPORT(chip_init)(void) {
   // The following message will appear in the browser's DevTools console:
   printf("Hello from custom chip!\n");
 
+  // Returning the chip instance is going away soon - but is still needed for some APIs (SPI, timers, pin_watch)
   return chip;
 }
 
 static void counter_updated(chip_state_t *chip) {
   const uint32_t threshold = attr_read(chip->threshold_attr);
   if (chip->counter > threshold) {
-    digital_write(chip->pin_int, LOW);
+    pin_write(chip->pin_int, LOW);
     pin_mode(chip->pin_int, OUTPUT);
   } else {
     pin_mode(chip->pin_int, INPUT);
   }
 }
 
-bool EXPORT(chip_i2c_connect)(chip_state_t *chip, uint32_t address, bool connect) {
+bool on_i2c_connect(void *user_ctx, uint32_t address, bool connect) {
   return true; /* Ack */
 }
 
-uint8_t EXPORT(chip_i2c_read)(chip_state_t *chip) {
+uint8_t on_i2c_read(void *user_ctx) {
+  chip_state_t *chip = user_ctx;
   chip->counter++;
   counter_updated(chip);
   return chip->counter;
 }
 
-bool EXPORT(chip_i2c_write)(chip_state_t *chip, uint8_t data) {
+bool on_i2c_write(void *user_ctx, uint8_t data) {
+  chip_state_t *chip = user_ctx;
   chip->counter = data;
   counter_updated(chip);
   return true; // Ack
 }
 
-void EXPORT(chip_i2c_disconnect)(chip_state_t *chip) {
+void on_i2c_disconnect(void *user_ctx) {
   // Do nothing
 }
