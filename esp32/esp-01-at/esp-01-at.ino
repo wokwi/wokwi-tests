9d76be62925f7787778cd38f50b693e8b92ec2b9
Uri Shaked <uri@urishaked.com>
2022-05-19 00:34:39 +0300

    feat(esp-01-at): add test

---
+++ esp32/esp-01-at/esp-01-at.ino
@@ -0,0 +1,35 @@
+/*
+  AT Firmware simulation example
+
+  Use the serial monitor to interface with the AT firmware. For instance, to scan for WiFi:
+
+AT+CWMODE=1
+AT+CWLAP
+
+  Connect to WiFi:
+
+AT+CWJAP="Wokwi-GUEST",
+
+  Show the current IP address:
+
+AT+CIFSR
+
+  Show firmware version
+
+AT+GMR
+
+  To show list list of available commands:
+
+AT+CMD?
+
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
