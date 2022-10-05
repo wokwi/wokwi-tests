50ad0c0fdbf047f812cd7776881c4090a7b18033
Uri Shaked <uri@urishaked.com>
2022-10-06 00:09:39 +0300

    fix(custom-chip): update code to latest API
    See migration guide: https://gist.github.com/urish/ce7ba1018d38cac58d72d83774a26023

--- arduino/custom-chip/i2c-counter.chip.c
+++ arduino/custom-chip/i2c-counter.chip.c
@@ -10,30 +10,25 @@
 
 const int ADDRESS = 0x22;
 
-DEFINE_PIN(VCC);
-DEFINE_PIN(GND);
-DEFINE_PIN(SCL);
-DEFINE_PIN(SDA);
-DEFINE_PIN(INT);
-
 typedef struct {
   pin_t pin_int;
   uint8_t counter;
   uint32_t threshold_attr;
 } chip_state_t;
 
-static bool on_i2c_connect(void *chip, uint32_t address, bool connect);
-static uint8_t on_i2c_read(void *chip);
-static bool on_i2c_write(void *chip, uint8_t data);
-static void on_i2c_disconnect(void *chip);
+static bool on_i2c_connect(void *user_data, uint32_t address, bool connect);
+static uint8_t on_i2c_read(void *user_data);
+static bool on_i2c_write(void *user_data, uint8_t data);
+static void on_i2c_disconnect(void *user_data);
 
-void* chip_init() {
+void chip_init() {
   chip_state_t *chip = malloc(sizeof(chip_state_t));
 
   chip->pin_int = pin_init("INT", INPUT);
   chip->counter = 0;
 
-  const i2c_dev_t i2c_dev = {
+  const i2c_config_t i2c_config = {
+    .user_data = chip,
     .address = ADDRESS,
     .scl = pin_init("SCL", INPUT),
     .sda = pin_init("SDA", INPUT),
@@ -42,16 +37,13 @@ void* chip_init() {
     .write = on_i2c_write,
     .disconnect = on_i2c_disconnect, // Optional
   };
-  i2c_init(chip, &i2c_dev);
+  i2c_init(&i2c_config);
 
   // This attribute can be edited by the user. It's defined in wokwi-custom-part.json:
   chip->threshold_attr = attr_init("threshold", 127);
 
   // The following message will appear in the browser's DevTools console:
   printf("Hello from custom chip!\n");
-
-  // Returning the chip instance is going away soon - but is still needed for some APIs (SPI, timers, pin_watch)
-  return chip;
 }
 
 static void counter_updated(chip_state_t *chip) {
@@ -64,24 +56,24 @@ static void counter_updated(chip_state_t *chip) {
   }
 }
 
-bool on_i2c_connect(void *user_ctx, uint32_t address, bool connect) {
+bool on_i2c_connect(void *user_data, uint32_t address, bool connect) {
   return true; /* Ack */
 }
 
-uint8_t on_i2c_read(void *user_ctx) {
-  chip_state_t *chip = user_ctx;
+uint8_t on_i2c_read(void *user_data) {
+  chip_state_t *chip = user_data;
   chip->counter++;
   counter_updated(chip);
   return chip->counter;
 }
 
-bool on_i2c_write(void *user_ctx, uint8_t data) {
-  chip_state_t *chip = user_ctx;
+bool on_i2c_write(void *user_data, uint8_t data) {
+  chip_state_t *chip = user_data;
   chip->counter = data;
   counter_updated(chip);
   return true; // Ack
 }
 
-void on_i2c_disconnect(void *user_ctx) {
+void on_i2c_disconnect(void *user_data) {
   // Do nothing
 }
